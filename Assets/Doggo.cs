2d51f30a6b94801821a837d522d2d6a7d5e90077
Patrick Riordan <patrickriordan177@gmail.com>
2022-01-29 11:22:58 -0800

    Add new character sprite and doggo sprite

--- Assets/Doggo.cs
+++ Assets/Doggo.cs
@@ -7,11 +7,18 @@ public class Doggo : MonoBehaviour
 {
     GameObject doggo;
     GameObject waypoint;
+    public Animator anim;
+    private Vector2 prevPos; // for calculating animation
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Start()
     {
         doggo = GameObject.Find("Doggo");
         waypoint = GameObject.Find("WayPointDoggo");
+        prevPos = transform.position;
     }
 
     void Update()
@@ -23,5 +30,30 @@ public class Doggo : MonoBehaviour
 
             transform.position = Vector2.MoveTowards(doggo.transform.position, waypoint.transform.position, 2*Time.deltaTime);
         }
+        Vector2 pos = transform.position;
+        Vector2 velocity = pos - prevPos;
+        if (velocity.x == 0 && velocity.y == 0)
+        {
+            anim.Play("idle");
+        } else if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
+        {
+            if (velocity.x > 0)
+            {
+                anim.Play("walkright");
+            } else
+            {
+                anim.Play("walkleft");
+            }
+        } else
+        {
+            if (velocity.y > 0)
+            {
+                anim.Play("walkup");
+            } else
+            {
+                anim.Play("walkdown");
+            }
+        }
+        prevPos = transform.position;
     }
 }
