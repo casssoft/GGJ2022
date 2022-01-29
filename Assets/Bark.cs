feae57284a3ef8940dec23fb69641f8c661d828f
Matthew Beaty <mattchasebeaty+git@gmail.com>
2022-01-29 10:52:01 -0700

    added basic bark bullets

---
+++ Assets/Bark.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bark : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Doggo.cs
+++ Assets/Doggo.cs
@@ -7,11 +7,17 @@ public class Doggo : MonoBehaviour
 {
     GameObject doggo;
     GameObject waypoint;
+    public GameObject barkPrefab;
+    GameObject sibling;
+    public float delayBetweenBarks = 3;
+    float lastBark;
 
     void Start()
     {
         doggo = GameObject.Find("Doggo");
+        sibling = GameObject.Find("Sibling");
         waypoint = GameObject.Find("WayPointDoggo");
+        lastBark = Time.time;
     }
 
     void Update()
@@ -32,10 +38,27 @@ public class Doggo : MonoBehaviour
             if (frisbees.Length > 0) {
                 GameObject frisbee = frisbees[0];
 
-                if ((transform.position - frisbee.transform.position).magnitude < 5)
-                transform.position = Vector2.MoveTowards(doggo.transform.position, frisbee.transform.position, 3*Time.deltaTime);
+                if ((transform.position - frisbee.transform.position).magnitude < 5) {
+                    transform.position = Vector2.MoveTowards(doggo.transform.position, frisbee.transform.position, 3*Time.deltaTime);
+                }
             }
         }
- 
+
+        // Doggo barks at sibling
+        if (GlobalVariables.followPlayer) {
+
+            if ((transform.position - sibling.transform.position).magnitude < 8) {
+                Bark();
+            }
+        }
+
+        void Bark(){
+            if (Time.time - lastBark > delayBetweenBarks) {
+                GameObject bark = Instantiate(barkPrefab, doggo.transform.position, this.gameObject.transform.rotation);
+                bark.tag = "Bark";
+
+                lastBark = Time.time;
+            }
+        }
     }
 }
